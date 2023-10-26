d68038c3582532ed2588a5e120ab94ef4ac4e962
IsaacDJohnson <isaac.designws@gmail.com>
2023-10-26 10:22:46 -0500

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+// create your App component here
+import React, {useEffect, useState} from 'react'
+
+function App(){
+
+    const [dogs, setDogs] = useState(<p>Loading...</p>)
+
+    useEffect(()=>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(r => r.json())
+        .then(data => setDogs(<img src={data.message} alt='A Random Dog'/>))
+    }, [])
+
+    return (
+        <div>
+            {dogs}
+        </div>
+    )
+}
+
+export default App;
